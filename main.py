d2b1b90ee3c51efb49684642ed8e6adc0bafaad0
melisk12 <129781468+melisk12@users.noreply.github.com>
2024-02-26 02:59:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,3 @@
-
-###############################################################
-# Gözetimsiz Öğrenme ile Müşteri Segmentasyonu (Customer Segmentation with Unsupervised Learning)
-###############################################################
-
-###############################################################
-# İş Problemi (Business Problem)
-###############################################################
-
-# Unsupervised Learning yöntemleriyle (Kmeans, Hierarchical Clustering )  müşteriler kümelere ayrılıp ve davranışları gözlemlenmek istenmektedir.
-
-###############################################################
-# Veri Seti Hikayesi
-###############################################################
-
-# Veri seti son alışverişlerini 2020 - 2021 yıllarında OmniChannel(hem online hem offline) olarak yapan müşterilerin geçmiş alışveriş davranışlarından
-# elde edilen bilgilerden oluşmaktadır.
-
-# 20.000 gözlem, 13 değişken
-
-# master_id: Eşsiz müşteri numarası
-# order_channel : Alışveriş yapılan platforma ait hangi kanalın kullanıldığı (Android, ios, Desktop, Mobile, Offline)
-# last_order_channel : En son alışverişin yapıldığı kanal
-# first_order_date : Müşterinin yaptığı ilk alışveriş tarihi
-# last_order_date : Müşterinin yaptığı son alışveriş tarihi
-# last_order_date_online : Muşterinin online platformda yaptığı son alışveriş tarihi
-# last_order_date_offline : Muşterinin offline platformda yaptığı son alışveriş tarihi
-# order_num_total_ever_online : Müşterinin online platformda yaptığı toplam alışveriş sayısı
-# order_num_total_ever_offline : Müşterinin offline'da yaptığı toplam alışveriş sayısı
-# customer_value_total_ever_offline : Müşterinin offline alışverişlerinde ödediği toplam ücret
-# customer_value_total_ever_online : Müşterinin online alışverişlerinde ödediği toplam ücret
-# interested_in_categories_12 : Müşterinin son 12 ayda alışveriş yaptığı kategorilerin listesi
-# store_type : 3 farklı companyi ifade eder. A company'sinden alışveriş yapan kişi B'dende yaptı ise A,B şeklinde yazılmıştır.
-
-
-###############################################################
-# GÖREVLER
-###############################################################
-
-# GÖREV 1: Veriyi Hazırlama
-           # 1. flo_data_20K.csv.csv verisini okuyunuz.
-           # 2. Müşterileri segmentlerken kullanacağınız değişkenleri seçiniz. Tenure(Müşterinin yaşı), Recency (en son kaç gün önce alışveriş yaptığı) gibi yeni değişkenler oluşturabilirsiniz.
-
-# GÖREV 2: K-Means ile Müşteri Segmentasyonu
-           # 1. Değişkenleri standartlaştırınız.
-           # 2. Optimum küme sayısını belirleyiniz.
-           # 3. Modelinizi oluşturunuz ve müşterilerinizi segmentleyiniz.
-           # 4. Herbir segmenti istatistiksel olarak inceleyeniz.
-
-# GÖREV 3: Hierarchical Clustering ile Müşteri Segmentasyonu
-           # 1. Görev 2'de standırlaştırdığınız dataframe'i kullanarak optimum küme sayısını belirleyiniz.
-           # 2. Modelinizi oluşturunuz ve müşterileriniz segmentleyiniz.
-           # 3. Herbir segmenti istatistiksel olarak inceleyeniz.
-
-
-###############################################################
-# GÖREV 1: Veri setini okutunuz ve müşterileri segmentlerken kullanıcağınız değişkenleri seçiniz.
-###############################################################
-
 import pandas as pd
 from scipy import stats
 import datetime as dt
@@ -75,28 +16,26 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 pd.set_option('display.width', 1000)
 
-
 df_ = pd.read_csv("Ödevler/6._HAFTA_ML/FLO_Unsupervised_Learning/dataset.csv")
 df = df_.copy()
 
-# tarih değişkenine çevirme
 date_columns = df.columns[df.columns.str.contains("date")]
 df[date_columns] = df[date_columns].apply(pd.to_datetime)
 
 df["last_order_date"].max() # 2021-05-30
 analysis_date = dt.datetime(2021,6,1)
 
-df["recency"] = (analysis_date - df["last_order_date"]).astype('timedelta64[D]') # en son kaç gün önce alışveriş yaptı
+df["recency"] = (analysis_date - df["last_order_date"]).astype('timedelta64[D]') 
 df["tenure"] = (df["last_order_date"]-df["first_order_date"]).astype('timedelta64[D]')
 
 model_df = df[["order_num_total_ever_online","order_num_total_ever_offline","customer_value_total_ever_offline","customer_value_total_ever_online","recency","tenure"]]
 model_df.head()
 
 ###############################################################
-# GÖREV 2: K-Means ile Müşteri Segmentasyonu
+# Customer Segmentation with K-Means
 ###############################################################
 
-# 1. Değişkenleri standartlaştırınız.
+# We standardize the variables.
 #SKEWNESS
 def check_skew(df_skew, column):
     skew = stats.skew(df_skew[column])
@@ -123,7 +62,7 @@ plt.tight_layout()
 plt.savefig('before_transform.png', format='png', dpi=1000)
 plt.show()
 
-# Normal dağılımın sağlanması için Log transformation uygulanması
+# Applying Log transformation to ensure normal distribution
 model_df['order_num_total_ever_online']=np.log1p(model_df['order_num_total_ever_online'])
 model_df['order_num_total_ever_offline']=np.log1p(model_df['order_num_total_ever_offline'])
 model_df['customer_value_total_ever_offline']=np.log1p(model_df['customer_value_total_ever_offline'])
@@ -139,14 +78,14 @@ model_df=pd.DataFrame(model_scaling,columns=model_df.columns)
 model_df.head()
 
 
-# 2. Optimum küme sayısını belirleyiniz.
+# Determining the optimal number of clusters
 kmeans = KMeans()
 elbow = KElbowVisualizer(kmeans, k=(2, 20))
 elbow.fit(model_df)
 elbow.show()
 
 
-# 3. Modelinizi oluşturunuz ve müşterilerinizi segmentleyiniz.
+# Model and Segment Srocess
 k_means = KMeans(n_clusters = 7, random_state= 42).fit(model_df)
 segments=k_means.labels_
 segments
@@ -155,8 +94,6 @@ final_df = df[["master_id","order_num_total_ever_online","order_num_total_ever_o
 final_df["segment"] = segments
 final_df.head()
 
-
-# 4. Herbir segmenti istatistiksel olarak inceleyeniz.
 final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","max"],
                                   "order_num_total_ever_offline":["mean","min","max"],
                                   "customer_value_total_ever_offline":["mean","min","max"],
@@ -166,10 +103,9 @@ final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","ma
 
 
 ###############################################################
-# GÖREV 3: Hierarchical Clustering ile Müşteri Segmentasyonu
+# Customer Segmentation with Hierarchical Clustering
 ###############################################################
 
-# 1. Görev 2'de standarlaştırdığınız dataframe'i kullanarak optimum küme sayısını belirleyiniz.
 hc_complete = linkage(model_df, 'complete')
 
 plt.figure(figsize=(7, 5))
@@ -183,7 +119,7 @@ plt.axhline(y=1.2, color='r', linestyle='--')
 plt.show()
 
 
-# 2. Modelinizi oluşturunuz ve müşterileriniz segmentleyiniz.
+# Model and Segment Srocess
 hc = AgglomerativeClustering(n_clusters=5)
 segments = hc.fit_predict(model_df)
 
@@ -191,7 +127,6 @@ final_df = df[["master_id","order_num_total_ever_online","order_num_total_ever_o
 final_df["segment"] = segments
 final_df.head()
 
-# 3. Her bir segmenti istatistiksel olarak inceleyeniz.
 final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","max"],
                                   "order_num_total_ever_offline":["mean","min","max"],
                                   "customer_value_total_ever_offline":["mean","min","max"],
